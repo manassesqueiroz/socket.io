5e84d4eaa3cb5214454f2f9d1aa7dad39b647279
manassesqueiroz <manassescruzgt@gmail.com>
2024-01-06 22:51:01 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { Server as typeServe ,createServer } from "http";
+import { Server } from "socket.io";
+
+class App {
+    public app: express.Application
+    public httpServer: typeServe
+    private io: Server;
+
+    constructor() {
+        this.app = express()
+        this.httpServer = createServer(this.app)
+        this.io = new Server(this.httpServer,{cors:{
+            origin: '*' // or client url exemplo 'http://localhost:3000' 
+        }})
+
+        this.io.on("connection", (socket) => {
+            console.log(`user:  ${socket.id}`)
+            socket.on('message' , (message) => {
+                
+                console.log( 'passando aqui',message)
+                socket.broadcast.emit('message', message)
+            })
+
+            socket.on("disconnect", (reason) => {
+                console.log(`socket ${socket.id} disconnected due to ${reason}`);
+              });
+        })
+    }
+    
+}
+
+const app = new App
+
+app.httpServer.listen(3333, () => {
+    console.log('Rodando')
+})
